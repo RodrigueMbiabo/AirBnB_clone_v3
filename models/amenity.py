d46686dd865bf579a595d6339d81ac1b212cba1a
rodriguembiabo <rodriguembiabo@gmail.com>
2022-10-23 23:47:53 +0200

    airbnb_clone_v3

---
+++ models/amenity.py
@@ -0,0 +1,24 @@
+#!/usr/bin/python
+""" holds class Amenity"""
+import models
+from models.base_model import BaseModel, Base
+from os import getenv
+import sqlalchemy
+from sqlalchemy import Column, String
+from sqlalchemy.orm import relationship
+
+
+class Amenity(BaseModel, Base):
+    """Representation of Amenity """
+    if models.storage_t == 'db':
+        __tablename__ = 'amenities'
+        name = Column(String(128), nullable=False)
+        __table_args__ = (
+                {'mysql_default_charset': 'latin1'}
+                )
+    else:
+        name = ""
+
+    def __init__(self, *args, **kwargs):
+        """initializes Amenity"""
+        super().__init__(*args, **kwargs)
